34ff5ad3df853f972a4420e053e401cfcbb67fc0
PREMz <PREMz@DESKTOP-OKJJHK3>
2021-04-19 16:41:18 +0700

    add drop card

--- src/studyjavafx/GameGUIController.java
+++ src/studyjavafx/GameGUIController.java
@@ -11,6 +11,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
+import javafx.scene.control.CheckBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -24,7 +25,9 @@ public class GameGUIController implements Initializable {
     //@FXML key and Variable
     @FXML private ImageView imgView1;
     @FXML private HBox hboxy;
+    @FXML private HBox hbox2;
     private ArrayList<ImageView> imageViewCardOnHands;
+    private ArrayList<CheckBox> checkBoxs;
     private Deck mainDeck;
     private Deck playerDeck;
     int x = 0;
@@ -41,6 +44,8 @@ public class GameGUIController implements Initializable {
         
         playerDeck = new Deck();
         
+        checkBoxs = new ArrayList<>();
+        
         imageViewCardOnHands = new ArrayList<ImageView>();
         imgView1.setImage(new Image("studyjavafx/images/backOfCard.jpg"));
         
@@ -56,19 +61,52 @@ public class GameGUIController implements Initializable {
     @FXML
     public void sortCardOnHands(){
         playerDeck.sortDeck();
-        //updateCardOnHands();
+        updateCardOnHand();
+    }
+    
+    @FXML public void dropCard(){
+        int check = 0;
+        for(CheckBox checkBox : checkBoxs)
+            if(checkBox.isSelected())
+                check++;
+        if(check == 2){
+            for (int i = 0; i < checkBoxs.size(); i++) {
+                boolean done = false;
+                for (int j = 0; j < checkBoxs.size(); j++) {
+                    if(i!=j && checkBoxs.get(i).isSelected() && checkBoxs.get(j).isSelected())
+                    {
+                        if(playerDeck.getDeck().get(i).getSuit() == playerDeck.getDeck().get(j).getSuit())
+                        {
+                            System.out.println("i=" + i + " j=" + j);
+                            System.out.println(playerDeck.getDeck().remove(j).getSuit());
+                            System.out.println(playerDeck.getDeck().remove(i).getSuit());
+                            done = true;
+                            break;
+                        }
+                    }
+                }
+                if(done) break;
+            }
+        }
+        updateCardOnHand();
     }
     
     private void LoadGameForOneBots(){
-        for(int i = 0; i < mainDeck.getSize()/2;i++){
+        int size = 26;//scale>10
+        for(int i = 0; i < size;i++){
             Card cardDraw = mainDeck.draw();
             playerDeck.addCard(cardDraw);
             imageViewCardOnHands.add(new ImageView(cardDraw.getImage()));
-            imageViewCardOnHands.get(i).setFitWidth(1080/26); //width of window /26
-            imageViewCardOnHands.get(i).setFitHeight(1600/26);//heigh of window /26
+            imageViewCardOnHands.get(i).setFitWidth(1100/size); //width = 1200/imageCount
+            imageViewCardOnHands.get(i).setFitHeight(726*1100/size/500);// height = 726*1200/imageCount/500
+            checkBoxs.add(new CheckBox());
         }
         hboxy.setAlignment(Pos.CENTER);
         hboxy.getChildren().addAll(imageViewCardOnHands);
+        
+        hbox2.setAlignment(Pos.CENTER);
+        hbox2.setSpacing(29);
+        hbox2.getChildren().addAll(checkBoxs);
          /**Click On imgView1 to move Down
         imgView1.setImage(new Image("studyjavafx/images/backOfCard.jpg"));
         imgView1.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
@@ -80,6 +118,36 @@ public class GameGUIController implements Initializable {
             }
         });*/
          
+    }
+    
+    private void updateCardOnHand(){
+        imageViewCardOnHands.clear();
+        hboxy.getChildren().clear();
+        hbox2.getChildren().clear();
+        checkBoxs.clear();
+        int size = playerDeck.getSize();
+        for(int i = 0; i< size; i++){
+            imageViewCardOnHands.add(new ImageView(playerDeck.getDeck().get(i).getImage()));
+            imageViewCardOnHands.get(i).setFitWidth(1100/size); //width of window /26
+            imageViewCardOnHands.get(i).setFitHeight(726*1100/size/500);//heigh of window /26
+            checkBoxs.add(new CheckBox());
+        }
+        setSpaceHbox2(size);
+        hboxy.getChildren().addAll(imageViewCardOnHands);
+        hbox2.getChildren().addAll(checkBoxs);
+        
+    }
+    
+    private void setSpaceHbox2(int size){
+        if(size == 26)          hbox2.setSpacing(29);
+        else if(size == 24)     hbox2.setSpacing(32);
+        else if(size == 22)     hbox2.setSpacing(37);
+        else if(size == 20)     hbox2.setSpacing(42);
+        else if(size == 18)     hbox2.setSpacing(48);
+        else if(size == 16)     hbox2.setSpacing(55);
+        else if(size == 14)     hbox2.setSpacing(65);
+        else 
+            hbox2.setSpacing(0);
     }
     
     
